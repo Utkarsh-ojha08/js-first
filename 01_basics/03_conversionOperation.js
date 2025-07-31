484c25ffbe94b3b06bd1eccee52f6b31e82565c4
UTKARSH OJHA <utkarshojha2007@gmail.com>
2025-07-31 11:01:51 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)//N of Number must be capital
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 =. false
+//"" => false
+// "utkarsh" => true
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
