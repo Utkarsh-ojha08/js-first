33a9d8071aef8848b83740d658581a67a9f663b8
UTKARSH OJHA <utkarshojha2007@gmail.com>
2025-07-31 10:39:00 +0000

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2
+accountEmail = "abcd@gmail.com"
+accountPassword = "21212121"
+accountCity = "delhi"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+
+
