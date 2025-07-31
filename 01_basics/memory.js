a26a98b34f1c6883dfeeb6c56a5035f212f995d7
UTKARSH OJHA <utkarshojha2007@gmail.com>
2025-07-31 12:25:53 +0000

    memory

---
+++ 01_basics/memory.js
@@ -0,0 +1,28 @@
+// Stack(Primitive), Heap(non-Primitive)
+
+let myName = "Utkarsh Ojha"
+
+let anotherName = myName
+anotherName = "Prachi"
+
+console.log(myName);
+console.log(anotherName);
+
+
+let userOne ={
+    email: "abc@google.com",
+    upi: "abc@oksbi"
+
+}
+
+let userTwo = userOne
+
+userTwo.email = "prachi@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
+
